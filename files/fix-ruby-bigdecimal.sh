946da0e0194e5f7ba42bcdac0674f6513daaa22b
Hax4us <30441647+Hax4us@users.noreply.github.com>
2019-02-28 11:59:59 +0530

    Create fix-ruby-bigdecimal.sh

---
+++ files/fix-ruby-bigdecimal.sh
@@ -0,0 +1,21 @@
+#!/data/data/com.termux/files/usr/bin/sh
+##
+##  Fix linking error for Ruby Bigdecimal
+##  native extensions.
+##
+
+apt install -yq patchelf
+
+for i in aarch64-linux-android arm-linux-androideabi \
+    i686-linux-android x86_64-linux-android; do
+
+    if [ -e "$PREFIX/lib/ruby/2.6.0/${i}/bigdecimal.so" ]; then
+        if [ -n "$(patchelf --print-needed "$PREFIX/lib/ruby/2.6.0/${i}/bigdecimal/util.so" | grep bigdecimal.so)" ]; then
+            exit 0
+        fi
+
+        patchelf --add-needed \
+            "$PREFIX/lib/ruby/2.6.0/${i}/bigdecimal.so" \
+            "$PREFIX/lib/ruby/2.6.0/${i}/bigdecimal/util.so"
+    fi
+done
